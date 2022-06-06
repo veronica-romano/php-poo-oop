be6f0c3ceee5248716b2bd4faa467fff57a367b3
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-06 11:07:19 -0300

    first file created
    simple oop examples

---
+++ exemplo-geral.php
@@ -0,0 +1,20 @@
+<?php
+
+class Exemplo{
+    public $nome;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO</title>
+</head>
+<body>
+    
+</body>
+</html>
