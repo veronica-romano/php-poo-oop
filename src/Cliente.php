f26c33e7f832f22113172573d42300fa5e7b72d0
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-06 11:54:33 -0300

    Update Cliente.php

--- src/Cliente.php
+++ src/Cliente.php
@@ -6,5 +6,13 @@ class Cliente{
     public string $email;
     public string $senha;
     public array $telefones;
+
+    //método construtor
+    public function __construct( string $nome, string $email)
+    {
+        // this é uma variável especial que referencia a classe atualmente manipulada.
+        $this->nome = $nome;
+        $this->email = $email;
+    }
 }
 ?>
