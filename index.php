fbb5ff8d3ae06c827d26ae158248ed3b15b2ab55
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-08 09:20:44 -0300

    Acesso direto aos recursos
    Atribuição e leitura de dados em propriedades e métodos públicos.

--- index.php
+++ index.php
@@ -4,29 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POO - Exemplo 2</title>
+    <title>POO - Exemplo 3</title>
 </head>
 <body>
-    <h1>PHP POO Exemplo 2</h1>
+    <h1>PHP POO Exemplo 3</h1>
     <hr>
     <h2>Assuntos abordados:</h2>
     <ul>
-        <li>Criação dos Objetos</li>
-        <li>Uso do método construtor</li>
-        <li>uso do <code>$this</code> para acessar as propriedades</li>
-        
+        <li>Acesso direto a propriedades</li>
+        <li>Atribuição de dados e leitura</li>     
     </ul>
 
     <?php
     require_once "src/Cliente.php";
+    // criação de objetos
+    $clienteA = new Cliente;
+    $clienteB = new Cliente;
 
-    $clienteA = new Cliente('Glauber', 'gcantacini@gmail.com');
-    $clienteB = new Cliente('Veronica', 'veronica.lima.silva@uni9.edu.br');
-    
+    // atribuindo dados às propriedades do objeto 
+
+    $clienteA->nome = "Glauber";
+    $clienteA->email = "gcantacini@gmail.com";
+    $clienteA->telefones = ["+5511997370011"];
+    //$clientea->senha = "111222333";
+
+    $clienteB->nome = "Veronica";
+    $clienteB->email = "vlsveronica@gmail.com";
+    $clienteB->telefones = array("+5511977370011");
+    //$clienteB->senha = "aaabbbccc";
+
+
+
     ?>
 
-    <pre> <?= var_dump($clienteA, $clienteB)?> </pre>
+    <h2>Dados dos objetos</h2>
+    <h3> <?=$clienteA->nome?> </h3>
+    <p>e-mail: <?=$clienteA->email?></p>
+    <p>telefones: <?=implode(" / ", $clienteA->telefones)?></p>
+    <p>senha: <?=$clienteA->senha?></p>
 
+    <h3> <?=$clienteB->nome?> </h3>
+    <p>e-mail: <?=$clienteB->email?></p>
+    <p>telefone: <?=implode(" / ", $clienteB->telefones)?></p>
+    <p>senha: <?=$clienteB->senha?></p>
+
+    <h2>Chamando o método <code>exibirDados()</code> </h2>
+    <?= $clienteA->exibirDados()?>
+    <?= $clienteB->exibirDados()?>
 
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -4,17 +4,16 @@
 class Cliente{
     public string $nome;
     public string $email;
-    public string $senha;
+    public string $senha = "";
     public array $telefones;
-
-    //método construtor
-    public function __construct( string $nome, string $email)
-    {
-        // this é uma variável especial que referencia a classe atualmente manipulada.´
-        // o __construct é chamado quando você cria um objeto, função de execução automática
-        
-        $this->nome = $nome;
-        $this->email = $email;
+    public function exibirDados(){
+        echo "<h3> $this->nome </h3>";
+        echo "<ul>";
+        echo "<li> $this->email </li>";
+        echo "<li>". implode(" / ", $this->telefones). "</li>";
+        echo "</ul>";
     }
+
+ 
 }
 ?>
