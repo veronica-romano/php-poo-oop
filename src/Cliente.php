bfb38405c840ac1737983fd38c2a9952128a9781
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-08 08:27:48 -0300

    Update Cliente.php

--- src/Cliente.php
+++ src/Cliente.php
@@ -10,7 +10,9 @@ class Cliente{
     //método construtor
     public function __construct( string $nome, string $email)
     {
-        // this é uma variável especial que referencia a classe atualmente manipulada.
+        // this é uma variável especial que referencia a classe atualmente manipulada.´
+        // o __construct é chamado quando você cria um objeto, função de execução automática
+        
         $this->nome = $nome;
         $this->email = $email;
     }
